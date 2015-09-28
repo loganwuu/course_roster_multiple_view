d8c140f032404e30a1f555511b82f19619d94b25
Logan Wu & Filipe Silvestre <student@epicodus.com>
2015-09-28 09:56:48 -0700

    add header home and update app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+var courseRoster = angular.module('courseRoster', ['ui.router']);
+
+courseRoster.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    views: {
+        'header': {
+          templateUrl: "partials/header.html",
+          controller: 'HeadersCtrl'
+      },
+        'body': {
+          templateUrl: "partials/home.html"
+      },
+    }
+  });
+
+  $stateProvider.state('courses', {
+    url: "/courses",
+    views: {
+        'header': {
+          templateUrl: "partials/header.html"
+      },
+        'body': {
+          templateUrl: "partials/courses.html",
+          controller: 'CoursesCtrl'
+      },
+    }
+  });
+
+  $stateProvider.state('courses.students', {
+    url: "/:courseId",
+    views: {
+        'header': {
+          templateUrl: "partials/header.html"
+      },
+        'body': {
+          templateUrl: "partials/courses.students.html",
+          controller: 'StudentsCtrl'
+      },
+    }
+  });
+});
